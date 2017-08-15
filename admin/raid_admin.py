647bdf2eda4fa613f6d018836810af4e87fb6d70
Ryan Anderson <ryana@tech-connect.com.au>
2017-08-15 10:47:47 +1000

    Add institution AWS Lambda CRUD Handler and IAM Policy
    Add institution AWS Lambda CRUD Handler to 'raid_admin.py' and IAM
    Policy with Dynambo DB permissions to template.yaml

--- admin/raid_admin.py
+++ admin/raid_admin.py
@@ -2,15 +2,23 @@ from __future__ import print_function
 
 import os
 import sys
+import re
 import logging
 import datetime
 import jwt
 import boto3
 
+# Roles
 INSTITUTION_ROLE = "institution"
 SERVICE_ROLE = "service"
-INSTITUTION_TABLE = os.environ["INSTITUTION_TABLE"]
 
+# Token issuing
+JWT_ISSUER = os.environ["JWT_ISSUER"]
+JWT_AUDIENCE = os.environ["JWT_AUDIENCE"]
+JWT_SECRET = os.environ["JWT_SECRET"]
+
+#Dynamo DB
+INSTITUTION_TABLE = os.environ["INSTITUTION_TABLE"]
 
 # Set Logging Level
 logger = logging.getLogger()
@@ -53,8 +61,103 @@ def jwt_role_encode(jwt_secret, jwt_audience, jwt_issuer, subject, role, months=
 
 
 def institution_crud_handler(event, context):
+    """
+    CRUD handler for institutions table
+    :param event: 
+    :param context: 
+    :return: 
+    """
     try:
         logger.info('Institution CRUD Event={}'.format(event))
+
+        # validate event
+        if "query" not in event or "parameters" not in event:
+            raise Exception("Event must contain the institution 'query' and 'parameters'.")
+
+        # Initialise DynamoDB
+        dynamodb = boto3.resource('dynamodb')
+        institution_table = dynamodb.Table(INSTITUTION_TABLE)
+
+        # Create new item
+        if event["query"] == "create":
+            # Validate parameter existence
+            if "name" not in event["parameters"] or "grid" not in event["parameters"]:
+                raise Exception("'name' and 'grid' must be provided in 'parameters to generate a JWT token.")
+
+            # TODO regex validate grid id 'grid.[NUMERIC].[ALPHANUMERIC]'
+
+            # Get current datetime
+            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            # Create JWT token
+            jwt = jwt_role_encode(JWT_SECRET, JWT_AUDIENCE, JWT_ISSUER, event["parameters"]["grid"], INSTITUTION_ROLE,
+                                  24)
+
+            # Define item
+            item = {
+                    'Name': event["parameters"]["name"],
+                    'Grid': event["parameters"]["grid"],
+                    'Date': now,
+                    'Token': jwt
+            }
+
+            # Send Dynamo DB put response
+            institution_table.put_item(Item=item)
+
+            return item
+
+        # Use existing item
+        elif event["query"] == "update" or event["query"] == "read" or event["query"] == "delete":
+            # Validate required item key values
+            if "grid" not in event["parameters"] or "date" not in event["parameters"]:
+                raise Exception("Key fields 'grid' and 'date' must be provided in 'parameters' for this query.")
+
+            if event["query"] == "update":
+                update_response = institution_table.update_item(
+                    Key={
+                        'Grid': event["parameters"]["grid"],
+                        'Date': event["parameters"]["date"]
+                    },
+                    UpdateExpression="set #token = :t",
+                    ExpressionAttributeValues={
+                        ':t': jwt_role_encode(JWT_SECRET, JWT_AUDIENCE, JWT_ISSUER, event["parameters"]["grid"],
+                                              INSTITUTION_ROLE, 24)
+                    },
+                    ExpressionAttributeNames={
+                        '#token': "Token",
+                    },
+                    ReturnValues="UPDATED_NEW"
+                )
+
+                return update_response["Attributes"]
+
+            elif event["query"] == "read":
+                read_response = institution_table.get_item(
+                    Key={
+                        'Grid': event["parameters"]["grid"],
+                        'Date': event["parameters"]["date"]
+                    }
+                )
+
+                if "Item" not in read_response:
+                    raise Exception("No item found matching provided keys.")
+
+                return read_response["Item"]
+
+            elif event["query"] == "delete":
+                if "grid" not in event["parameters"] or "date" not in event["parameters"]:
+                    raise Exception("'grid' and 'date' must be provided in 'parameters to generate a new JWT token.")
+
+                delete_response = institution_table.delete_item(
+                    Key={
+                        'Grid': event["parameters"]["grid"],
+                        'Date': event["parameters"]["date"]
+                    }
+                )
+
+        else:
+            raise Exception("Invalid query type '{}'."
+                            " Only 'create', 'update', 'read', and 'delete' may be used.".format(event["query"]))
     except:
         logger.error('Unexpected error: {}'.format(sys.exc_info()[0]))
         raise
