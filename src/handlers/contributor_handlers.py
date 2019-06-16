c6050faa84930c1e9609ed6ca2f0bd778272b02c
Ryan Anderson <ryan.dan.anderson@gmail.com>
2019-06-16 16:11:03 +1000

    Add API and functions to add a contributor to a RAiD

---
+++ src/handlers/contributor_handlers.py
@@ -0,0 +1,186 @@
+import os
+import sys
+import logging
+import datetime
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
+import json
+import urllib
+from helpers import web_helpers
+from helpers import raid_helpers
+import settings
+
+# Set Logging Level
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+
+def add_contributor(event, context):
+    """
+    Add/Invite a contributor to a RAiD via email or an existing active Orcid user
+    :param event:
+    :param context:
+    :return: {"message": ""}
+    """
+    if 'requestContext' not in event:
+        return {"message": "Warming Lambda container"}
+
+    try:
+        raid_handle = urllib.unquote(urllib.unquote(event["pathParameters"]["raidId"]))
+
+    except:
+        logger.error('Unable to validate RAiD parameter: {}'.format(sys.exc_info()[0]))
+        return web_helpers.generate_web_body_response(
+            '400',
+            {'message': "Incorrect path parameter type formatting for RAiD handle. Ensure it is a URL encoded string"},
+            event
+        )
+
+    try:
+        environment = event['requestContext']['authorizer']['environment']
+        provider = event['requestContext']['authorizer']['provider']
+
+        # Initialise DynamoDB
+        dynamo_db = boto3.resource('dynamodb')
+        raid_table = dynamo_db.Table(
+            settings.get_environment_table(settings.RAID_TABLE, environment)
+        )
+        # Check if RAiD exists
+        query_response = raid_table.query(KeyConditionExpression=Key('handle').eq(raid_handle))
+
+        if query_response["Count"] != 1:
+            return web_helpers.generate_web_body_response('400', {
+                'message': "Invalid RAiD handle provided in parameter path. "
+                           "Ensure it is a valid RAiD handle URL encoded string"}, event)
+
+        # Assign raid item to single item, since the result will be an array of one item
+        raid_item = query_response['Items'][0]
+
+        # Interpret and validate request body
+        if 'body' in event and event["body"]:
+            body = json.loads(event["body"])
+
+            if ('email' in body) and ('orcid' in body) or ('email' not in body) and ('orcid' not in body):
+                return web_helpers.generate_web_body_response(
+                    '400',
+                    {'message': 'Invalid request body: Either an "email" or "orcid" must be provided.'},
+                    event
+                )
+
+            elif 'email' or 'orcid' in body:
+                # Create core contributor item for invitation or Orcid record processing
+                now = raid_helpers.get_current_datetime()  # Get current datetime
+                contributor_item = {'creationDate': now, 'handle': raid_handle, 'provider': provider}
+
+                if "startDate" in body:  # Default to the current data and time
+                    try:
+                        start_date = body["startDate"]
+                        datetime.datetime.strptime(start_date, "%Y-%m-%d %H:%M:%S")
+                        contributor_item['startDate'] = start_date
+
+                    except ValueError as e:
+                        logger.error('Unable to capture date: {}'.format(e))
+                        return web_helpers.generate_web_body_response('400', {
+                            'message': "Incorrect date format, should be yyyy-MM-dd hh:mm:ss"}, event)
+
+                else:
+                    contributor_item['startDate'] = raid_helpers.get_current_datetime()
+
+                if "role" in body:  # Default to "Contributor"
+                    contributor_item['role'] = body['role']
+                else:
+                    contributor_item['role'] = 'Contributor'
+
+                if "description" in body:  # Default to the existing RAiD Description
+                    contributor_item['description'] = body['description']
+                else:
+                    contributor_item['description'] = 'Contributor to the RAiD project "{}"'.format(
+                        raid_item['meta']['name'])
+
+                if 'email' in body:  # Invite a contributor to a RAiD and the
+                    contributor_invitation_table = dynamo_db.Table(
+                        settings.get_environment_table(settings.CONTRIBUTOR_INVITATIONS_TABLE, environment)
+                    )
+
+                    # Check if there is an existing invitation
+                    query_response = contributor_invitation_table.query(
+                        KeyConditionExpression=Key('email').eq(body['email']),
+                        FilterExpression=Attr('handle').eq(raid_handle)
+                    )
+
+                    if query_response["Count"] > 0:
+                        return web_helpers.generate_web_body_response('400', {
+                            'message': "There is an existing invitation to this contributor."}, event)
+
+                    # Save Invitation to Contributor Invitations Table
+                    contributor_item['email'] = body['email']
+                    contributor_invitation_table.put_item(Item=contributor_item)
+
+                    # TODO Send SES Email
+                    return web_helpers.generate_web_body_response(
+                        '200',
+                        {'message': 'An email invite has been sent to the contributor.'},
+                        event
+                    )
+
+                else:  # Invite a contributor to a RAiD via their integrated orcid
+                    contributors_table = dynamo_db.Table(
+                        settings.get_environment_table(settings.CONTRIBUTORS_TABLE, environment)
+                    )
+
+                    # Check it is an active Orcid user
+                    query_response = contributors_table.query(
+                        KeyConditionExpression=Key('orcid').eq(body['orcid'])
+                    )
+
+                    if query_response["Count"] < 1:
+                        return web_helpers.generate_web_body_response('400', {
+                            'message': "This Orcid user has not granted RAiD permission to be associated."}, event)
+
+                    contributor_item['orcid'] = body['orcid']
+                    contributor_item['type'] = 'add'  # The type or Orcid Interaction
+
+                    # Send contributor information to SQS Queue
+                    if environment == settings.LIVE_ENVIRONMENT:
+                        logger.info('Sending message on Orcid Interaction SQS Queue...')
+                        queue = os.environ["ORCID_INTERACTION_QUEUE"]
+
+                    else:  # Use demo queue if not in the Live environment
+                        logger.info('Sending message on Demo Orcid Interaction SQS Queue...')
+                        queue = os.environ["DEMO_ORCID_INTERACTION_QUEUE"]
+
+                    SQS_client = boto3.client('sqs')
+                    send_response = SQS_client.send_message(
+                        QueueUrl=queue,
+                        MessageBody=json.dumps(contributor_item)
+                    )
+
+                    return web_helpers.generate_web_body_response(
+                        '200',
+                        {'message': 'A request to add active Orcid member as a Contributor will be processed.'},
+                        event
+                    )
+
+            else:
+                return web_helpers.generate_web_body_response(
+                    '400',
+                    {'message': 'Invalid request body: Either an "email" or "orcid" must be provided.'},
+                    event
+                )
+
+        else:
+            return web_helpers.generate_web_body_response(
+                '400',
+                {'message': 'Invalid request body: Either an "email" or "orcid" must be provided.'},
+                event
+
+            )
+
+    except Exception as e:
+        logger.error('Unable to add contributor: {}'.format(e))
+        return web_helpers.generate_web_body_response(
+            '400',
+            {'message': "Unable to add contributor. Please check structure of the JSON body."},
+            event
+        )
--- src/settings.py
+++ src/settings.py
@@ -5,6 +5,9 @@ INSTITUTION_ROLE = "institution"
 SERVICE_ROLE = "service"
 RAID_TABLE = "RAID_TABLE"
 ASSOCIATION_TABLE = "ASSOCIATION_TABLE"
+CONTRIBUTORS_TABLE = "CONTRIBUTORS_TABLE"
+CONTRIBUTOR_INVITATIONS_TABLE = "CONTRIBUTOR_INVITATIONS_TABLE"
+RAID_CONTRIBUTORS_TABLE = "RAID_CONTRIBUTORS_TABLE"
 DEMO_ENVIRONMENT = "demo"
 LIVE_ENVIRONMENT = "live"
 RAID_SITE_URL = "https://www.raid.org.au/"
@@ -29,3 +32,18 @@ def get_environment_table(table_name, environment):
             return os.environ["ASSOCIATION_DEMO_TABLE"]
         elif environment == LIVE_ENVIRONMENT:
             return os.environ["ASSOCIATION_TABLE"]
+    elif table_name == CONTRIBUTORS_TABLE:
+        if environment == DEMO_ENVIRONMENT:
+            return os.environ["CONTRIBUTORS_DEMO_TABLE"]
+        elif environment == LIVE_ENVIRONMENT:
+            return os.environ["CONTRIBUTORS_TABLE"]
+    elif table_name == CONTRIBUTOR_INVITATIONS_TABLE:
+        if environment == DEMO_ENVIRONMENT:
+            return os.environ["CONTRIBUTOR_INVITATIONS_DEMO_TABLE"]
+        elif environment == LIVE_ENVIRONMENT:
+            return os.environ["CONTRIBUTOR_INVITATIONS_TABLE"]
+    elif table_name == RAID_CONTRIBUTORS_TABLE:
+        if environment == DEMO_ENVIRONMENT:
+            return os.environ["RAID_CONTRIBUTORS_DEMO_TABLE"]
+        elif environment == LIVE_ENVIRONMENT:
+            return os.environ["RAID_CONTRIBUTORS_TABLE"]
