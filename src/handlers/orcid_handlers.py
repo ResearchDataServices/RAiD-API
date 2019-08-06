9197ae6abd667e12a6c74760e7768f490d85298b
Ryan Anderson <ryan.dan.anderson@gmail.com>
2019-08-06 11:54:53 +1000

    Add API endpoint to end a RAiD contributor

--- src/handlers/orcid_handlers.py
+++ src/handlers/orcid_handlers.py
@@ -61,34 +61,39 @@ def process_queue(event, context):
                 # Create Orcid Member API object and request body
                 api = orcid_helpers.get_orcid_api_object(environment=environment)
                 access_token = contributor['access_token']
-                orcid_json = orcid_helpers.queue_record_to_orcid_request_object(body)
 
-                # Process Orcid CRUD
-                if body['type'] == 'add':
-                    logger.info('Adding a new ORCID Record for id:{}...'.format(contributor['orcid']))
+                if body['type'] == 'add' or body['type'] == 'update':
+                    orcid_json = orcid_helpers.queue_record_to_orcid_request_object(body)
 
-                    if raid_contributor is not None and any(activity['endDate'] is None for activity in raid_contributor['activities']):  # TODO Check there is not an activity still in progress
-                        logger.error('This Orcid user is already an active contributor.')
-                        continue
+                    # Process Orcid CRUD
+                    if body['type'] == 'add':
+                        logger.info('Adding a new ORCID Record for id:{}...'.format(contributor['orcid']))
 
-                    # Send request to Orcid
-                    put_code = api.add_record(contributor['orcid'], access_token, 'work', orcid_json)
+                        if raid_contributor is not None and any(activity['endDate'] is None for activity in raid_contributor['activities']):
+                            logger.error('This Orcid user is already an active contributor.')
+                            continue
 
-                    # Save new contributor association to DynamoDB
-                    contributors_helpers.create_or_update_raid_contributor(body, put_code, environment=environment)
+                        # Send request to Orcid
+                        put_code = api.add_record(contributor['orcid'], access_token, 'work', orcid_json)
 
-                elif body['type'] == 'update':
-                    logger.info('Updating an ORCID Record for id:{}...'.format(contributor['orcid']))
+                        # Save new contributor association to DynamoDB
+                        contributors_helpers.create_or_update_raid_contributor(body, put_code, environment=environment)
 
-                    # Send request to Orcid
-                    api.update_record(
-                        contributor['orcid'], access_token, 'work', raid_contributor['putCode'], orcid_json
-                    )
+                    elif body['type'] == 'update':  # TODO
+                        logger.info('Updating an ORCID Record for id:{}...'.format(contributor['orcid']))
 
-                    # Update new contributor association to DynamoDB
-                    contributors_helpers.create_or_update_raid_contributor(
-                        body, raid_contributor['putCode'], environment=environment
-                    )
+                        # Send request to Orcid
+                        api.update_record(
+                            contributor['orcid'], access_token, 'work', raid_contributor['putCode'], orcid_json
+                        )
+
+                        # Update new contributor association to DynamoDB
+                        contributors_helpers.create_or_update_raid_contributor(
+                            body, raid_contributor['putCode'], environment=environment
+                        )
+
+                elif body['type'] == 'delete':  # TODO
+                    logger.info('Ending an ORCID Record association for id:{}...'.format(contributor['orcid']))
 
                 else:
                     logger.error('Unknown type')
@@ -103,3 +108,4 @@ def process_queue(event, context):
         logger.error(str(e))
 
     logger.info('Orcid SQS Queue Processed...')
+
